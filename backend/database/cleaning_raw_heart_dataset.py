7e97a688845b8c00ddaf34b4033812894a51e1d4
drkchn <57195399+drkchn@users.noreply.github.com>
2023-10-28 15:58:03 -0700

    Feature selection done

--- backend/database/cleaning_raw_heart_dataset.py
+++ backend/database/cleaning_raw_heart_dataset.py
@@ -10,9 +10,9 @@ filtered_data = data[["age", "sex", "cp", "trtbps", "chol", "fbs", "restecg",
 
 print(filtered_data.head())
 
-# one-hot encode the age column so age above 70 gets (1) and 70 and under gets (0)
+# one-hot encode the age column so age between 55-75 gets (0), and other ages get (1)
 filtered_data = filtered_data.assign(
-    Age_encoded=filtered_data['age'].apply(lambda x: 1 if x >= 50 else 0))
+    Age_encoded=filtered_data['age'].apply(lambda x: 0 if (x >= 55) & (x <= 75) else 1))
 
 # one-hot encode the sex column
 filtered_data = filtered_data.assign(
@@ -23,7 +23,7 @@ filtered_data = filtered_data.assign(
     Cp_encoded=filtered_data['cp'].apply(lambda x: 1 if x > 1 else 0))
 
 # one-hot encode the cholesterol column so high cholesterol gets (1) and normal cholesterol gets (0)
-mean_chol = filtered_data['chol'].quantile(0.70)
+mean_chol = filtered_data['chol'].mean()
 filtered_data = filtered_data.assign(
     Cholesterol_encoded=filtered_data['chol'].apply(lambda x: 1 if x >= mean_chol else 0))
 
@@ -32,23 +32,27 @@ print(filtered_data[['chol', 'Cholesterol_encoded']].head(30))
 
 # one-hot encode the blood pressure based on the following: if the row has high blood pressure (1),
 # otherwise, encode (0)
-mean_trtbps = filtered_data['trtbps'].quantile(0.50)
+mean_trtbps = filtered_data['trtbps'].mean()
 filtered_data = filtered_data.assign(
     BP_encoded=filtered_data['trtbps'].apply(lambda x: 1 if x >= mean_trtbps else 0))
 
-# one-hot encode the blood pressure based on the following: if the row has high blood pressure (1),
-# otherwise, encode (0)
+# one-hot encode the ecg based on the following: if the row has normal ecg (0),
+# otherwise, encode (1)
 filtered_data = filtered_data.assign(
     Restecg_encoded=filtered_data['restecg'].apply(lambda x: 1 if x >= 1 else 0))
 
+# one-hot encode the exercise induced chest pain column so chest pain gets (1) and no chest pain gets (0)
+filtered_data = filtered_data.assign(
+    Exng_encoded=filtered_data['exng'].apply(lambda x: 1 if x == 0 else 0))
 
-# rename columns
+
+# rename target prediction column
 filtered_data.rename(columns={'output': 'Heart_Attack_Risk'}, inplace=True)
 
 # grab new columns for the export dataframe
 filtered_data = filtered_data[
     ["Age_encoded", "Sex_encoded", "Cholesterol_encoded", "Cp_encoded", "BP_encoded", "fbs", "Restecg_encoded",
-     "Heart_Attack_Risk"]]
+     "Exng_encoded", "Heart_Attack_Risk"]]
 
 
 # shift heart attack risk column to the right most column
@@ -60,14 +64,14 @@ print(filtered_data.head(30))
 # create correlation matrix for feature selection
 filtered_data = filtered_data[
     ["Age_encoded", "Sex_encoded", "Cholesterol_encoded", "Cp_encoded", "BP_encoded", "fbs", "Restecg_encoded",
-     "Heart_Attack_Risk"]]
+     "Exng_encoded", "Heart_Attack_Risk"]]
 print(filtered_data.corr())
 
 # feature selection
-#filtered_data = filtered_data[
-#    ["Age_encoded", "Sex_encoded", "Cholesterol_encoded", "cp_encoded", "BP_encoded", "fbs", "restecg",
-#     "Heart_Attack_Risk"]]
-#print(filtered_data.corr())
+filtered_data = filtered_data[
+    ["Age_encoded", "Sex_encoded", "Cp_encoded", "Restecg_encoded",
+     "Exng_encoded", "Heart_Attack_Risk"]]
+print(filtered_data.corr())
 
 # export dataframe to csv
 filtered_data.to_csv("cleaned_heart_attack_data.csv", index=False)
