32b8afafeea9e4367879cec3dc38c44b473d0888
Megha Jain <meghajain@berkeley.edu>
2023-10-29 11:43:54 -0700

    Fixed up Results page [FINAL]

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -33,7 +33,8 @@ export default function Home() {
                 </div>
                 <div>
                     <h2 className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert text-2xl font-sans font-semibold text-center bg-gradient-to-r from-red-400 to-red-800 bg-clip-text text-transparent leading-tight">
-                        You should choose Plan B
+                        You have a high risk of heart attacks <br/>
+                        and should choose Plan B.
                     </h2>
                     <h2 className="mt-20 relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert text-l font-sans text-center bg-gray-600 bg-clip-text text-transparent leading-tight">
                         Plan A has a lower monthly premium and a higher deductible compared to Plan B. This means that the patient will pay less each month for their insurance, but will have to pay more out-of-pocket before their insurance coverage kicks in. This could be a good option for someone who is generally healthy and does not anticipate needing frequent medical care. On the other hand, Plan B has a higher monthly premium but a lower deductible. This means that the patient will pay more each month for their insurance, but will have a lower out-of-pocket cost when they do need medical care. This could be a better option for someone who has a higher likelihood of needing medical treatment, such as someone with a high probability of suffering a heart attack. 
