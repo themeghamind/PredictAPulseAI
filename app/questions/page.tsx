2cacf25cd4fa09d3c9b4997bfc311f367eb6f5a0
drkchn <57195399+drkchn@users.noreply.github.com>
2023-10-29 01:52:22 -0700

    React states to change values on button click

--- app/questions/page.tsx
+++ app/questions/page.tsx
@@ -1,10 +1,36 @@
 "use client"
 
+import { useState } from 'react';
 import Image from 'next/image'
 import Link from 'next/link'
 
 export default function Questions() {
-    return (
+
+  const [checkboxValues, setCheckboxValues] = useState({
+    gender: false,
+    age: false,
+    chol: false,
+    bp: false,
+    chest_pains: false,
+    chest_pains_exercise: false,
+    ecg: false,
+  });
+
+  const handleCheckboxChange = (event: { target: { name: any; checked: any; }; }) => {
+    const { name, checked } = event.target;
+    setCheckboxValues((prevValues) => {
+      const updatedValues = {
+        ...prevValues,
+        [name]: checked,
+      };
+      console.log(updatedValues);
+
+      return updatedValues;
+    });
+  };
+
+
+  return (
     <div>
       <header style={{ paddingTop: '30px', paddingLeft: '80px' }}>
         <a
@@ -26,55 +52,72 @@ export default function Questions() {
       <main className="flex min-h-screen flex-col items-center justify-center p-24 mt-8">
         <div className="relative flex place-items-center mt-[-8rem] before:absolute before:h-[600px] before:w-[680px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 z-[-1]">
           <h1 className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert text-6xl font-sans font-semibold text-center bg-red-800 bg-clip-text text-transparent leading-tight">
-            Health Questionnare
+            Health Questionnaire
           </h1>
         </div>
         <div>
           <h2 className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert text-4xl font-sans font-semibold text-center bg-red-800 bg-clip-text text-transparent leading-tight">
-            Check either &rsquo;yes&rsquo; or &rsquo;no&rsquo; for each question
+            Select each question if it applies to you:
           </h2>
         </div>
         <div className="mt-10 w-3/6">
           <div className="flex items-center justify-between mb-10">
             <label className="text-xl font-sans text-gray-600">Are you male?</label>
             <div className="flex items-center gap-4">
-              <input type="checkbox" name="Gender" value="yes" style={{ transform: 'scale(1.3)' }}/>
+              <input type="checkbox"
+                     name="gender"
+                     value="yes"
+                     style={{ transform: 'scale(1.3)' }}
+                     checked={checkboxValues.gender}
+                     onChange={handleCheckboxChange}/>
             </div>
           </div>
           <div className="flex items-center justify-between mb-10 mt-4">
-            <label className="text-xl font-sans text-gray-600">Is your age above 60?</label>
+            <label className="text-xl font-sans text-gray-600">Are you above 55 years old?</label>
             <div className="flex items-center gap-4">
-              <input type="checkbox" name="Age" value="yes" style={{ transform: 'scale(1.3)' }}/>
+              <input type="checkbox" name="age" value="yes" style={{ transform: 'scale(1.3)' }}
+                     checked={checkboxValues.age}
+                     onChange={handleCheckboxChange}/>
             </div>
           </div>
           <div className="flex items-center justify-between mb-10 mt-4">
-            <label className="text-xl font-sans text-gray-600">Is your LDL, or bad cholesterol, above 60?</label>
+            <label className="text-xl font-sans text-gray-600">Do you have high cholesterol (above 200 mg/dL)?</label>
             <div className="flex items-center gap-4">
-              <input type="checkbox" name="LDL" value="yes" style={{ transform: 'scale(1.3)' }}/>
+              <input type="checkbox" name="chol" value="yes" style={{ transform: 'scale(1.3)' }}
+                     checked={checkboxValues.chol}
+                     onChange={handleCheckboxChange}/>
             </div>
           </div>
           <div className="flex items-center justify-between mb-10 mt-4">
-            <label className="text-xl font-sans text-gray-600">Do you smoke?</label>
+            <label className="text-xl font-sans text-gray-600">Do you have high blood pressure (above 130 mm Hg/80 mm Hg)?</label>
             <div className="flex items-center gap-4">
-              <input type="checkbox" name="Smoking" value="yes" style={{ transform: 'scale(1.3)' }}/>
+              <input type="checkbox" name="bp" value="yes" style={{ transform: 'scale(1.3)' }}
+                     checked={checkboxValues.bp}
+                     onChange={handleCheckboxChange}/>
             </div>
           </div>
           <div className="flex items-center justify-between mb-10 mt-4">
-            <label className="text-xl font-sans text-gray-600">Is your blood pressure above 130/80?</label>
+            <label className="text-xl font-sans text-gray-600">Do you experience chest pains?</label>
             <div className="flex items-center gap-4">
-              <input type="checkbox" name="BP" value="yes" style={{ transform: 'scale(1.3)' }}/>
+              <input type="checkbox" name="chest_pains" value="yes" style={{ transform: 'scale(1.3)' }}
+                     checked={checkboxValues.chest_pains}
+                     onChange={handleCheckboxChange}/>
             </div>
           </div>
           <div className="flex items-center justify-between mb-10 mt-4">
-            <label className="text-xl font-sans text-gray-600 mr-10">Do you currently have or ever had diabetes?</label>
+            <label className="text-xl font-sans text-gray-600 mr-10">Do you experience chest pains post-exercise?</label>
             <div className="flex items-center gap-4">
-              <input type="checkbox" name="Diabetes" value="yes" style={{ transform: 'scale(1.3)' }}/>
+              <input type="checkbox" name="chest_pains_exercise" value="yes" style={{ transform: 'scale(1.3)' }}
+                     checked={checkboxValues.chest_pains_exercise}
+                     onChange={handleCheckboxChange}/>
             </div>
           </div>
           <div className="flex items-center justify-between mb-10 mt-4">
-            <label className="text-xl font-sans text-gray-600">Are you currently or have ever been obese?</label>
+            <label className="text-xl font-sans text-gray-600">Has your physician informed you have an abnormal ECG reading?</label>
             <div className="flex items-center gap-4">
-              <input type="checkbox" name="Obesity" value="yes" style={{ transform: 'scale(1.3)' }}/>
+              <input type="checkbox" name="ecg" value="yes" style={{ transform: 'scale(1.3)' }}
+                     checked={checkboxValues.ecg}
+                     onChange={handleCheckboxChange}/>
             </div>
           </div>
         </div>
